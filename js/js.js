a10fff36b69d5bd48b1ed0f8b4766e0d52ef0bf6
Gustavo <gustavo-a@outlook.com>
2023-03-08 19:49:27 -0300

    primer commit

---
+++ js/js.js
@@ -0,0 +1,20 @@
+document.getElementById('icon_1').onmouseover = function () {
+    document.getElementById('demo').innerHTML="Juan Fernandez";
+}
+document.getElementById('icon_2').onmouseover = function () {
+    document.getElementById('demo').innerHTML="Av. Mitre 4550";
+}
+document.getElementById('icon_3').onmouseover = function () {
+    document.getElementById('demo').innerHTML="jfernandez@gmail.com";
+}
+document.getElementById('icon_4').onmouseover = function () {
+    document.getElementById('demo').innerHTML="+54 9 11 2258 6532";
+}
+document.getElementById('icon_5').onmouseover = function () {
+    document.getElementById('demo').innerHTML="16/09/1970";
+}
+document.getElementById('icon_6').onmouseover = function () {
+    document.getElementById('demo').innerHTML="Caseros, Buenos Aires";
+}
+
+
